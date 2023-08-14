79614fd677851f780fdef24316e2c04fe0bfb599
BoyleHere <prashast1509@gmail.com>
2023-08-14 09:17:33 +0530

    multiplication done

--- OOP2/Lab2/LAB2-Prashast/src/main/java/multiplication.java
+++ OOP2/Lab2/LAB2-Prashast/src/main/java/multiplication.java
@@ -14,7 +14,17 @@ use left shift and right shift bitwise operators]*/
 import java.util.Scanner;
 
 public class multiplication {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        int a,b;
+    System.out.println("Enter a number to be multiplied with 2 :");
+    Scanner sc = new Scanner(System.in);
+    a = sc.nextInt();
+    int anew = a<<1;
+    System.out.println("Enter a number to divided by 2: ");
+    b=sc.nextInt();
+    int bnew =b>>1;
+        System.out.println(a+" multiplied by 2 is " +anew);
+        System.out.println(b+" divided by 2 is "+bnew);
+    
     }
 }
